b77229e7955142d204a53bf215d95dfbdb9d392c
rsliu-225 <52770366+rsliu-225@users.noreply.github.com>
2021-06-25 23:43:03 +0900

    Revert "Revert "Merge branch 'local' into main""
    This reverts commit 8f979a522580bd8419364792f75da53ac1894fa0.

---
+++ 0000_students_work/2021tro/gen_rbt.py
@@ -0,0 +1,22 @@
+import robot_sim.robots.ur3e_dual.ur3e_dual as ur3ed
+import robot_sim.end_effectors.grippers.robotiqhe.robotiqhe as rtqhe
+import visualization.panda.world as wd
+import modeling.geometric_model as gm
+import modeling.collision_model as cm
+import basis.robot_math as rm
+import numpy as np
+import math
+
+if __name__ == '__main__':
+    import os
+
+    base = wd.World(cam_pos=[10, 0, 5], lookat_pos=[0, 0, 1])
+    gm.gen_frame().attach_to(base)
+    rbt = ur3ed.UR3EDual()
+    hnd = rtqhe.RobotiqHE()
+    # rbt.gen_meshmodel().attach_to(base)
+    hnd.gen_meshmodel().attach_to(base)
+
+    # hnd.grip_at_with_jcpose()
+    gm.gen_cone(radius=.05).attach_to(base)
+    base.run()
--- 0000_students_work/2021tro/projection.py
+++ 0000_students_work/2021tro/projection.py
@@ -1,59 +1,64 @@
-import numpy as np
-import modeling.geometric_model as gm
-import modeling.collision_model as cm
-import visualization.panda.world as wd
-import basis.robot_math as rm
 import math
+
+import numpy as np
 from scipy.spatial import cKDTree
 
-base = wd.World(cam_pos=np.array([-.2,-.7,.42]), lookat_pos=np.array([0,0,0]))
+import basis.robot_math as rm
+import modeling.collision_model as cm
+import modeling.geometric_model as gm
+import visualization.panda.world as wd
+
+base = wd.World(cam_pos=np.array([-.2, -.7, .42]), lookat_pos=np.array([0, 0, 0]))
 # gm.gen_frame().attach_to(base)
 bowl_model = cm.CollisionModel(initor="./objects/bowl.stl")
-bowl_model.set_rgba([.3,.3,.3,.3])
-bowl_model.set_rotmat(rm.rotmat_from_euler(math.pi,0,0))
+bowl_model.set_rgba([.3, .3, .3, .3])
+bowl_model.set_rotmat(rm.rotmat_from_euler(math.pi, 0, 0))
 bowl_model.attach_to(base)
 
 pn_direction = np.array([0, 0, -1])
 
 bowl_samples, bowl_sample_normals = bowl_model.sample_surface(toggle_option='normals', radius=.002)
-selection = bowl_sample_normals.dot(-pn_direction)>.1
+selection = bowl_sample_normals.dot(-pn_direction) > .1
 bowl_samples = bowl_samples[selection]
-bowl_sample_normals=bowl_sample_normals[selection]
+bowl_sample_normals = bowl_sample_normals[selection]
 tree = cKDTree(bowl_samples)
 
 pt_direction = rm.orthogonal_vector(pn_direction, toggle_unit=True)
 tmp_direction = np.cross(pn_direction, pt_direction)
 plane_rotmat = np.column_stack((pt_direction, tmp_direction, pn_direction))
-homomat=np.eye(4)
-homomat[:3,:3] = plane_rotmat
-homomat[:3,3] = np.array([-.07,-.03,.1])
-twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=homomat, rgba=[1,1,1,.3])
+homomat = np.eye(4)
+homomat[:3, :3] = plane_rotmat
+homomat[:3, 3] = np.array([-.07, -.03, .1])
+twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=homomat, rgba=[1, 1, 1, .3])
 twod_plane.attach_to(base)
 
-circle_radius=.05
-line_segs = [[homomat[:3,3], homomat[:3,3]+pt_direction*.05], [homomat[:3,3]+pt_direction*.05, homomat[:3,3]+pt_direction*.05+tmp_direction*.05]]
+circle_radius = .05
+line_segs = [[homomat[:3, 3], homomat[:3, 3] + pt_direction * .05],
+             [homomat[:3, 3] + pt_direction * .05, homomat[:3, 3] + pt_direction * .05 + tmp_direction * .05]]
+print(line_segs)
 gm.gen_linesegs(line_segs).attach_to(base)
 gm.gen_arrow(spos=line_segs[0][0], epos=line_segs[0][1], thickness=0.004).attach_to(base)
-spt = homomat[:3,3]
+spt = homomat[:3, 3]
 # gm.gen_stick(spt, spt + pn_direction * 10, rgba=[0,1,0,1]).attach_to(base)
 # base.run()
-gm.gen_dasharrow(spt, spt-pn_direction*.07, thickness=.004).attach_to(base) # p0
+gm.gen_dasharrow(spt, spt - pn_direction * .07, thickness=.004).attach_to(base)  # p0
 cpt, cnrml = bowl_model.ray_hit(spt, spt + pn_direction * 10000, option='closest')
-gm.gen_dashstick(spt, cpt, rgba=[.57,.57,.57,.7], thickness=0.003).attach_to(base)
+gm.gen_dashstick(spt, cpt, rgba=[.57, .57, .57, .7], thickness=0.003).attach_to(base)
 gm.gen_sphere(pos=cpt, radius=.005).attach_to(base)
-gm.gen_dasharrow(cpt, cpt-pn_direction*.07, thickness=.004).attach_to(base) # p0
-gm.gen_dasharrow(cpt, cpt+cnrml*.07, thickness=.004).attach_to(base) # p0
+gm.gen_dasharrow(cpt, cpt - pn_direction * .07, thickness=.004).attach_to(base)  # p0
+gm.gen_dasharrow(cpt, cpt + cnrml * .07, thickness=.004).attach_to(base)  # p0
 
 angle = rm.angle_between_vectors(-pn_direction, cnrml)
 vec = np.cross(-pn_direction, cnrml)
 rotmat = rm.rotmat_from_axangle(vec, angle)
 new_plane_homomat = np.eye(4)
-new_plane_homomat[:3,:3] = rotmat.dot(homomat[:3,:3])
-new_plane_homomat[:3,3] = cpt
-twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=new_plane_homomat, rgba=[1,1,1,.3])
+new_plane_homomat[:3, :3] = rotmat.dot(homomat[:3, :3])
+new_plane_homomat[:3, 3] = cpt
+twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=new_plane_homomat, rgba=[1, 1, 1, .3])
 twod_plane.attach_to(base)
-new_line_segs = [[cpt, cpt+rotmat.dot(pt_direction)*.05],
-                 [cpt+rotmat.dot(pt_direction)*.05, cpt+rotmat.dot(pt_direction)*.05+rotmat.dot(tmp_direction)*.05]]
+new_line_segs = [[cpt, cpt + rotmat.dot(pt_direction) * .05],
+                 [cpt + rotmat.dot(pt_direction) * .05,
+                  cpt + rotmat.dot(pt_direction) * .05 + rotmat.dot(tmp_direction) * .05]]
 gm.gen_linesegs(new_line_segs).attach_to(base)
 # gm.gen_arrow(spos=new_line_segs[0][0], epos=new_line_segs[0][1], thickness=0.004).attach_to(base)
 
@@ -61,10 +66,10 @@ t_cpt = cpt
 last_normal = cnrml
 direction = rotmat.dot(pt_direction)
 tmp_direction = rotmat.dot(tmp_direction)
-n=3
-for tick in range(1, n+1):
-    t_npt = cpt+direction*.05/n
-    gm.gen_arrow(spos=t_npt, epos=t_npt+last_normal*.015, thickness=0.001).attach_to(base)
+n = 3
+for tick in range(1, n + 1):
+    t_npt = cpt + direction * .05 / n
+    gm.gen_arrow(spos=t_npt, epos=t_npt + last_normal * .015, thickness=0.001).attach_to(base)
     nearby_sample_ids = tree.query_ball_point(t_npt, .005)
     nearby_samples = bowl_samples[nearby_sample_ids]
     gm.GeometricModel(nearby_samples).attach_to(base)
@@ -73,26 +78,26 @@ for tick in range(1, n+1):
     plane_tmp = np.cross(plane_normal, plane_tangential)
     plane_rotmat = np.column_stack((plane_tangential, plane_tmp, plane_normal))
     homomat = np.eye(4)
-    homomat[:3,:3]=plane_rotmat
-    homomat[:3,3]=plane_center
-    twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=homomat, rgba=[.5,.7,1,.3]).attach_to(base)
+    homomat[:3, :3] = plane_rotmat
+    homomat[:3, 3] = plane_center
+    twod_plane = gm.gen_box(np.array([.2, .2, .001]), homomat=homomat, rgba=[.5, .7, 1, .3]).attach_to(base)
     projected_point = rm.project_to_plane(t_npt, plane_center, plane_normal)
     # gm.gen_stick(t_npt, projected_point, thickness=.002).attach_to(base)
-    new_normal = rm.unit_vector(t_npt-projected_point)
-    gm.gen_arrow(spos=projected_point, epos=projected_point+new_normal*.015, thickness=0.001).attach_to(base)
+    new_normal = rm.unit_vector(t_npt - projected_point)
+    gm.gen_arrow(spos=projected_point, epos=projected_point + new_normal * .015, thickness=0.001).attach_to(base)
     angle = rm.angle_between_vectors(last_normal, new_normal)
     vec = rm.unit_vector(np.cross(last_normal, new_normal))
     new_rotmat = rm.rotmat_from_axangle(vec, angle)
     direction = new_rotmat.dot(direction)
     tmp_direction = new_rotmat.dot(tmp_direction)
-    cpt=projected_point
-    new_line_segs = [[cpt, cpt+direction*(.05-tick*.05/n)],
-                     [cpt+direction*(.05-tick*.05/n), cpt+direction*(.05-tick*.05/n)+tmp_direction*.05]]
+    cpt = projected_point
+    new_line_segs = [[cpt, cpt + direction * (.05 - tick * .05 / n)],
+                     [cpt + direction * (.05 - tick * .05 / n),
+                      cpt + direction * (.05 - tick * .05 / n) + tmp_direction * .05]]
     gm.gen_linesegs(new_line_segs).attach_to(base)
     last_normal = new_normal
     # break
 
 base.run()
-
 
 base.run()
