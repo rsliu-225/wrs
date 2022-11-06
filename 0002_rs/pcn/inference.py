73374e262dd32cdf182936685999c2f651650ec2
rsliu-225 <52770366+rsliu-225@users.noreply.github.com>
2022-11-06 19:56:46 +0900

    inference update

--- 0002_rs/pcn/inference.py
+++ 0002_rs/pcn/inference.py
@@ -3,6 +3,7 @@ import importlib
 import numpy as np
 import h5py
 import subprocess
+import os
 
 from numpy.lib.index_tricks import AxisConcatenator
 import munch
@@ -12,16 +13,16 @@ import open3d as o3d
 import warnings
 
 import config
+import pcn.models.pcn as model_module
 
 warnings.filterwarnings("ignore")
 
 
-def inference_sgl(input_data, model_name, load_model):
+def inference_sgl(input_narry, model_name='pcn', load_model='pcn_emd_prim_mv/best_cd_p_network.pth'):
+    load_model = os.path.join(config.ROOT, model_name, 'trained_model', load_model)
     device = torch.device('cpu')
     args = munch.munchify({'num_points': 2048, 'loss': 'cd', 'eval_emd': False})
-    input_data = np.asarray(input_data)
-    input_data = torch.from_numpy(input_data)
-    model_module = importlib.import_module('.%s' % model_name, 'models')
+    input_narry = torch.from_numpy(input_narry)
     net = torch.nn.DataParallel(model_module.Model(args))
     net.module.load_state_dict(torch.load(load_model, map_location=torch.device('cpu'))['net_state_dict'])
     net = net.module.to(device)
@@ -30,7 +31,7 @@ def inference_sgl(input_data, model_name, load_model):
 
     logging.info('Testing...')
     with torch.no_grad():
-        inputs_cpu = input_data
+        inputs_cpu = input_narry
         inputs = inputs_cpu.float().cpu()
         inputs = torch.unsqueeze(inputs, 0)
         inputs = inputs.transpose(2, 1).contiguous()
@@ -51,20 +52,24 @@ def nparray2o3dpcd(nx3nparray_pnts, nx3nparray_nrmls=None, estimate_normals=Fals
 
 
 if __name__ == "__main__":
+
     COLOR = np.asarray([[31, 119, 180], [44, 160, 44], [214, 39, 40]]) / 255
     f_name = 'test'
 
     model_name = 'pcn'
-    load_model = './trained_model/pcn_emd_prim_mv/best_cd_p_network.pth'
+    load_model = 'pcn_emd_prim_mv/best_cd_p_network.pth'
 
     # o3dpcd_1 = o3d.io.read_point_cloud('D:/liu/MVP_Benchmark/completion/data_real/020.pcd')
     # o3dpcd_2 = o3d.io.read_point_cloud('D:/liu/MVP_Benchmark/completion/data_real/001.pcd')
-    o3dpcd_1 = o3d.io.read_point_cloud(config.ROOT+'/recons_data/nbc/plate_a_cubic/000.pcd')
-    o3dpcd = o3dpcd_1
-    o3dpcd = o3dpcd.uniform_down_sample(int(len(np.asarray(o3dpcd.points)) / 2048))
-    print(np.asarray(o3dpcd.points).shape)
-    result = inference_sgl(np.asarray(o3dpcd.points), model_name, load_model)
-    o3dpcd_o = nparray2o3dpcd(result)
-    o3dpcd.paint_uniform_color(COLOR[0])
-    o3dpcd_o.paint_uniform_color(COLOR[2])
-    o3d.visualization.draw_geometries([o3dpcd, o3dpcd_o])
+    o3dpcd_1 = o3d.io.read_point_cloud(config.ROOT + '/recons_data/nbc/plate_a_cubic/000.pcd')
+    path = os.path.join(config.ROOT, 'recons_data/nbc/plate_a_cubic')
+    for f in os.listdir(path):
+        o3dpcd = o3d.io.read_point_cloud(os.path.join(path, f))
+        if len(np.asarray(o3dpcd.points)) > 2048:
+            o3dpcd = o3dpcd.uniform_down_sample(int(len(np.asarray(o3dpcd.points)) / 2048))
+        print(np.asarray(o3dpcd.points).shape)
+        result = inference_sgl(np.asarray(o3dpcd.points), model_name, load_model)
+        o3dpcd_o = nparray2o3dpcd(result)
+        o3dpcd.paint_uniform_color(COLOR[0])
+        o3dpcd_o.paint_uniform_color(COLOR[2])
+        o3d.visualization.draw_geometries([o3dpcd, o3dpcd_o])
